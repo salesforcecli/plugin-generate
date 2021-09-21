89f037b9173a781ee05afb8a3fc3344f5365ba58
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-09-21 17:53:49 -0400

    chore: add unit tests for generate project command

--- test/commands/generate/project.test.ts
+++ test/commands/generate/project.test.ts
@@ -4,3 +4,63 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+import { expect, use as chaiUse } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+
+import { stubMethod } from '@salesforce/ts-sinon';
+
+import GenerateProject from '../../../src/commands/generate/project';
+
+chaiUse(sinonChai);
+
+describe('./src/commands/generate/project.ts', () => {
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+
+    // reset process.argv
+    process.argv = process.argv.slice(0, 2);
+  });
+
+  describe('build', () => {
+    let runGeneratorStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      runGeneratorStub = stubMethod(sandbox, GenerateProject.prototype, 'runGenerator');
+      runGeneratorStub.resolves();
+
+      // This allows us to simulate running `sf generate project -n testProj` when calling GenerateProject.run() in our tests
+      process.argv[2] = '-n';
+      process.argv[3] = 'testProj';
+    });
+
+    it('should call .runGenerator()', async () => {
+      await GenerateProject.run();
+
+      expect(runGeneratorStub).to.have.been.calledOnce;
+    });
+
+    it('should call .runGenerator() with the command flags', async () => {
+      process.argv[3] = 'theBestProjectEver';
+      process.argv[4] = '-d';
+      process.argv[5] = 'theBestOutputDirEver';
+      process.argv[6] = '-p';
+      process.argv[7] = 'theBestPackageDirEver';
+
+      await GenerateProject.run();
+
+      // .args[0][1] corresponds to the first time runGeneratorStub is called and the second argument passed into that call
+      expect(runGeneratorStub.args[0][1]).to.deep.equal({
+        projectname: 'theBestProjectEver',
+        template: 'standard',
+        outputdir: 'theBestOutputDirEver',
+        defaultpackagedir: 'theBestPackageDirEver',
+        loginurl: 'https://login.salesforce.com',
+        ns: '',
+      });
+    });
+  });
+});

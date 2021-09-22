df3c51f00822bf8f7e7cd5667d9bb7ca0e30b47b
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-09-21 21:52:04 -0400

    chore: add basic nut

---
+++ test/commands/generate/project.nut.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import * as path from 'path';
+import { rm } from 'shelljs';
+import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { expect } from 'chai';
+import { Env } from '@salesforce/kit';
+import { GenerateOutput } from '../../../src/commands/generate/project';
+
+let testSession: TestSession;
+
+describe('generate project NUTs', () => {
+  const env = new Env();
+  env.setString('TESTKIT_EXECUTABLE_PATH', path.join(process.cwd(), 'bin', 'dev'));
+
+  before('prepare session and ensure environment variables', async () => {
+    testSession = await TestSession.create({ authStrategy: 'NONE' });
+  });
+
+  after(async () => {
+    await testSession?.clean();
+  });
+
+  afterEach(() => {
+    const testProjDir = path.join(process.cwd(), 'testProj');
+    rm('-rf', testProjDir);
+  });
+
+  it('generate a project', () => {
+    const command = 'generate project -n testProj';
+    const stdout = execCmd<GenerateOutput>(command, { cli: 'sf', ensureExitCode: 0 }).shellOutput.stdout;
+
+    expect(stdout).to.match(/create testProj/);
+  });
+});

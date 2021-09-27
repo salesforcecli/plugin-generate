0627856cc3e064b54ac799dd6135cecec07a6fee
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-09-27 12:51:17 -0400

    fix: apiVersion

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-export const defaultApiVersion = '52.1.0';
+
+// This should be the version that corresponds with the latest Salesforce Release.
+// You can find it at https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/intro_rest.htm
+export const defaultApiVersion = '53.0';
